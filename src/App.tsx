008cca2aa95d98f20435a85b6b9b665310c42c7c
Chizaram-Igolo <chizaram@yahoo.com>
2023-09-10 02:31:07 +0100

    🛠️ Fix Routing Bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
 
 import SignUpSuccess from "./pages/Authentication/SignUpSuccess";
 import SignUp from "./pages/Authentication/SignUp";
@@ -6,18 +7,34 @@ import SignIn from "./pages/Authentication/SignIn";
 import Dashboard from "./pages/Dashboard/Dashboard";
 
 import "./App.css";
+import Protected from "./pages/Authentication/Protected";
+
+// function Home() {
+
+// }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/register-success" element={<SignUpSuccess />} />
+  const token = localStorage.getItem("to-note") as string;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-        <Route path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      // setToken(localStorage.getItem("to-note") as string);
+      // navigate("/login");
+      setIsLoggedIn(true);
+    }
+  }, [token]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/register-success" element={<SignUpSuccess />} />
+      <Route path="/dashboard/*" element={<Dashboard />} />
+    </Routes>
   );
 }
 
